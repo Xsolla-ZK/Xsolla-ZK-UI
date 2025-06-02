01540c3a30f22f53f695d599d8f1a2d6ecd3ac1a
Yaroslav Volov <knuckleswolf@gmail.com>
2025-06-02 21:05:29 +0500

    fix: debug publish

--- packages/config/types/index.d.ts
+++ packages/config/types/index.d.ts
@@ -3,7 +3,7 @@ export { settings } from './settings';
 export { shorthands } from './shorthands';
 export { fonts } from './tokens/fonts';
 export { themes } from './tokens/themes';
-export { tokens } from './tokens';
+export { tokens } from './tokens/tokens';
 export { deepMerge, createTheme, composeThemes, createComponentTheme, } from './utils/create-component-theme';
 export { sharedConfig } from './shared';
 export { components } from './tokens/components';
